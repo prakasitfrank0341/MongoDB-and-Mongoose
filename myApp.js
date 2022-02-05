b8a3ac1042db519c3524b4d2034755f7aade0704
Prakasit Kobkeaw <prakasitfrank@gmail.com>
2022-02-05 22:57:26 +0700

    Use model to Updates by find-edit-save

--- myApp.js
+++ myApp.js
@@ -68,8 +68,15 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, function(error, person) {
+    if(error) return console.log(error);
+      person.favoriteFoods.push(foodToAdd);
+      
+      person.save((error, data) => {
+        if(error) return console.log(error);
+        done(null, data);
+      });
+  });
 };
 
 const findAndUpdate = (personName, done) => {
