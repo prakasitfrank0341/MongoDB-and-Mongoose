e9b9f176d8d66dd002affb40bad04489f6952d81
Prakasit Kobkeaw <prakasitfrank@gmail.com>
2022-02-08 14:02:09 +0700

    Uncomment code

--- myApp.js
+++ myApp.js
@@ -14,88 +14,88 @@ var personSchema = new Schema ({
 let Person = mongoose.model('Person', personSchema);
 
 
-//const createAndSavePerson = (done) => {
-//  let jaylorx = new Person({name: 'Jaylorx', age: 23, favoriteFoods: ['Cake']})
+const createAndSavePerson = (done) => {
+ let jaylorx = new Person({name: 'Jaylorx', age: 23, favoriteFoods: ['Cake']})
 
-//  jaylorx.save((error, data) => {
-//    if(error) {
-//      console.log(error);
-//    }else {
-//      done(null, data);
-//    }
-//  });
-//};
+ jaylorx.save((error, data) => {
+   if(error) {
+     console.log(error);
+   }else {
+     done(null, data);
+   }
+ });
+};
 
-//const createManyPeople = (arrayOfPeople, done) => {
-//  Person.create(arrayOfPeople, function(error, peopleCreated) {
-//    if(error) {
-//      return console.log(error);
-//    }else {
-//      done(null, peopleCreated);
-//    }
-//  });
-//};
+const createManyPeople = (arrayOfPeople, done) => {
+ Person.create(arrayOfPeople, function(error, peopleCreated) {
+   if(error) {
+     return console.log(error);
+   }else {
+     done(null, peopleCreated);
+   }
+ });
+};
 
-//const findPeopleByName = (personName, done) => {
-//  Person.find({name: personName}, function(error, peopleFound) {
-//    if(error) {
-//      return console.log(error);
-//    }else {
-//      done(null, peopleFound);
-//    }
-//  });
-//};
+const findPeopleByName = (personName, done) => {
+ Person.find({name: personName}, function(error, peopleFound) {
+   if(error) {
+     return console.log(error);
+   }else {
+     done(null, peopleFound);
+   }
+ });
+};
 
-//const findOneByFood = (food, done) => {
-//  Person.findOne({favoriteFoods: food}, function(error, foodThatPersonLike) {
-//    if(error) {
-//      return console.log(error);
-//    }else {
-//      done(null, foodThatPersonLike);
-//    }
-//  });
-//};
+const findOneByFood = (food, done) => {
+ Person.findOne({favoriteFoods: food}, function(error, foodThatPersonLike) {
+   if(error) {
+     return console.log(error);
+   }else {
+     done(null, foodThatPersonLike);
+   }
+ });
+};
 
-//const findPersonById = (personId, done) => {
-//  Person.findById(personId, function(error, individual) {
-//    if(error) {
-//      return console.log(error);
-//    }else {
-//      done(null, individual);
-//    }
-//  });
-//};
+const findPersonById = (personId, done) => {
+ Person.findById(personId, function(error, individual) {
+   if(error) {
+     return console.log(error);
+   }else {
+     done(null, individual);
+   }
+ });
+};
 
-//const findEditThenSave = (personId, done) => {
-//  const foodToAdd = "hamburger";
-//  Person.findById(personId, function(error, person) {
-//    if(error) return console.log(error);
-//      person.favoriteFoods.push(foodToAdd);
+const findEditThenSave = (personId, done) => {
+ const foodToAdd = "hamburger";
+ Person.findById(personId, function(error, person) {
+   if(error) return console.log(error);
+     person.favoriteFoods.push(foodToAdd);
       
-//      person.save((error, data) => {
-//        if(error) return console.log(error);
-//        done(null, data);
-//      });
-//  });
-//};
+     person.save((error, data) => {
+       if(error) return console.log(error);
+       done(null, data);
+     });
+ });
+};
 
-//const findAndUpdate = (personName, done) => {
-//  let query = {name: personName};
-//  let ageToSet = {age: 20};
-//  let option = {new: true};
+const findAndUpdate = (personName, done) => {
+ let query = {name: personName};
+ let ageToSet = {age: 20};
+ let option = {new: true};
 
-//  Person.findOneAndUpdate(query, ageToSet, option, (error, individual) => {
-//    if(error) return console.log(error);
-//    done(null, individual);
-//  });
-//};
+ Person.findOneAndUpdate(query, ageToSet, option, (error, individual) => {
+   if(error) return console.log(error);
+   done(null, individual);
+ });
+};
 
-//const removeById = (personId, done) => {
-//  Person.findByIdAndRemove(personId, (error, person) => {
-//    if(error) return console.log(error);
-//    done(null, person);
-//  });
-//};
+const removeById = (personId, done) => {
+ Person.findByIdAndRemove(personId, (error, person) => {
+   if(error) return console.log(error);
+   done(null, person);
+ });
+};
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
